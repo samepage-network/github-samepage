f28e2d3ca435af2122560643eeee01e24157f757
David Vargas <dvargas92495@gmail.com>
2023-07-03 23:31:56 -0400

    add assets

--- api/webhook/post.ts
+++ api/webhook/post.ts
@@ -1,8 +1,8 @@
 import createAPIGatewayHandler from "samepage/backend/createAPIGatewayProxyHandler";
 
-const logic = (args: { action: "created" | "deleted" }) => {
+const logic = (args: { action: "created" | "deleted"; sender: unknown }) => {
   // TODO - verify the webhook is from GitHub and delete related notebook
-  console.log("WEBHOOK", Object.keys(args));
+  console.log("WEBHOOK", args.action, args.sender, Object.keys(args));
   return {};
 };
 
